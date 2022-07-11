418bb1581f64991c3f122939ce2091eb20d5f5f1
fat_panda <1030397448@qq.com>
2022-07-12 00:26:22 +0800

    add framework

---
+++ Frameworkone/Frameworkone.h
@@ -0,0 +1,18 @@
+//
+//  Frameworkone.h
+//  Frameworkone
+//
+//  Created by iyf on 2022/7/12.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Frameworkone.
+FOUNDATION_EXPORT double FrameworkoneVersionNumber;
+
+//! Project version string for Frameworkone.
+FOUNDATION_EXPORT const unsigned char FrameworkoneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Frameworkone/PublicHeader.h>
+
+
